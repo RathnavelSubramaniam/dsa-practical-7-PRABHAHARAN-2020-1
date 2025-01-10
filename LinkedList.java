a0cc6c57dcf8a90e6180b21a67c27723f7682e6c
PRABHAHARAN A <122508061+PRABHAHARAN-2020@users.noreply.github.com>
2025-01-10 09:26:45 +0000

    cmt2

--- LinkedList.java
+++ LinkedList.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+
 public class LinkedList {
     class Node {
         int data;
         Node next;
 
-public Node(int data) {
-this.data = data;
-this.next = null;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
 
@@ -18,61 +19,51 @@ this.next = null;
         if (head == null) {
             head = newNode;
             tail = newNode;
-        } 
-else 
-{
-tail.next = newNode;
-tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
     }
-
-public static void main(String[] args) {
+   
+    public static void main(String[] args) {
         LinkedList list = new LinkedList();
         Scanner sc = new Scanner(System.in);
+        
         int pos=0;
         int data,data1;
         System.out.println("ENTER INTEGERS TO CREATE NODES (ENTER A NON-INTEGER TO EXIT):");
-        while (sc.hasNext())
-        {
-         
-            if(sc.hasNextInt())
-            {
-            data = sc.nextInt();
-            list.addNode(data);
+
+        while (sc.hasNext()) {
+            if(sc.hasNextInt()) {
+                data = sc.nextInt();
+                list.addNode(data);
+            } else {
+                String temp=sc.next();
+                System.out.println("LINKED LIST CREATED");
+                System.out.println("LINKED LIST BEFORE INSERTING");
+                list.displayList();
+                System.out.println("ENTER THE POSIITON WHERE YOU WANT TO INSERT");
+                if(sc.hasNextInt()) {
+                    pos=sc.nextInt();
+                    System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
+                    if(sc.hasNextInt()) {
+                        data1=sc.nextInt();
+                        list.insertMiddle(data1,pos);
+                        System.out.println("LINKED LIST AFTER INSERTING");
+                        list.displayList();
+                    } else {
+                        System.out.println("Enter integers only");
+                        System.exit(0);
+                    }
+                } else {
+                    System.out.println("Enter integers only");
+                    System.exit(0);
+                }
             }
-          else
-          {
-            String temp=sc.next();
-            System.out.println("LINKED LIST CREATED");
-            System.out.println("LINKED LIST BEFORE INSERTING");
-            list.displayList();
-            System.out.println("ENTER THE POSITON WHERE YOU WANT TO INSERT");
-            if(sc.hasNextInt())
-            {
-            pos=sc.nextInt();
-            System.out.println("ENTER THE ELEMENT TO BE INSERTED:");
-            if(sc.hasNextInt())
-            {
-            data1=sc.nextInt();
-            list.insertMiddle(data1,pos);
-            System.out.println("LINKED LIST AFTER INSERTING");
-            list.displayList();
-            }
-            else
-            {
-              System.out.println("Enter integers only");
-              System.exit(0);
-            }
-            }
-            else
-            {
-              System.out.println("Enter integers only");
-              System.exit(0);
-            }
-           }
         }
     }
-public void insertMiddle(int data, int pos) {
+
+    public void insertMiddle(int data, int pos) {
         Node newNode = new Node(data);
         if (pos == 1) {
             newNode.next = head;
@@ -96,12 +87,13 @@ public void insertMiddle(int data, int pos) {
         }
         System.out.println("ELEMENT INSERTED");
     }
-public void displayList() {
+
+    public void displayList() {
         Node current = head;
         while (current != null) {
-        System.out.print(current.data + " -> ");
-        current = current.next;
+            System.out.print(current.data + " -> ");
+            current = current.next;
         }
-System.out.println("null");
+        System.out.println("null");
     }
 }
